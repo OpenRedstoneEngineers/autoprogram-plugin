75665468cefb56f2d64633092f5a438fec455b17
mort <martid0311@gmail.com>
2015-04-08 18:22:34 +0200

    A player now needs the permission node autoprogram.exec to use the command.

--- src/main/java/org/openredstone/autoprogram/AutoProgram.java
+++ src/main/java/org/openredstone/autoprogram/AutoProgram.java
@@ -12,6 +12,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.permissions.Permission;
 
 public class AutoProgram extends JavaPlugin implements Listener
 {
@@ -54,6 +55,13 @@ public class AutoProgram extends JavaPlugin implements Listener
 		}
 
 		Player player = (Player)sender;
+
+		//Check permissions and such
+		if (!player.hasPermission("autoprogram.exec"))
+		{
+			sender.sendMessage("You don't have permission to do that.");
+			return false;
+		}
 
 		//Autoprogram requires exactly one argument.
 		if (args.length != 1)
